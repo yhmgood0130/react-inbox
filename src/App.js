42dc09cce5d38edbf0ca6cbbe7b168fdd0193ff5
Hyunmo <yhmgood0130@gmail.com>
2017-09-05 15:37:07 -0600

    API added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import Toolbar from "./components/Header/Toolbar"
 import Messages from "./components/Messages"
-import MessageData from "./components/MessageData"
 import './App.css';
+
+const baseURL = 'https://inbox-react.herokuapp.com/api'
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = { messages: MessageData, newSub: ""}
+  constructor() {
+    super()
+    this.state = { messages: [], newSub: ""}
   };
+
+  componentDidMount () {
+    fetch(`${baseURL}/messages`)
+      .then(data => data.json())
+      .then(response =>{
+        this.setState({messages: response._embedded.messages});
+      })
+  }
 
   checkbox (message){
     this.setState((prevState) => {
@@ -23,18 +32,55 @@ export default class App extends Component {
   emailCheck (message){
     this.setState((prevState) => {
       const currentBodyStatus = prevState.messages.indexOf(message);
-      prevState.messages[currentBodyStatus].bodyStatus = !prevState.messages[currentBodyStatus].bodyStatus
+      prevState.messages[currentBodyStatus].bodyStatus = !prevState.messages[currentBodyStatus].bodyStatus;
     })
     this.setState((prevState) => {
       const currentRead = prevState.messages.indexOf(message);
-      prevState.messages[currentRead].read = true
+      prevState.messages[currentRead].read = true;
+      const data = {
+        "messageIds": [message.id],
+        "command" : "read",
+        "read" : true
+      };
+      const settings = {
+        method: 'PATCH',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      };
+      fetch(`${baseURL}/messages`,settings)
+        .then(response => {
+          if(response.ok){
+            console.log(response);
+          }
+        })
     })
   };
   star (message){
     this.setState((prevState) => {
       const currentStar = prevState.messages.indexOf(message);
       prevState.messages[currentStar].starred = !prevState.messages[currentStar].starred
+      const data = {
+        "messageIds": [message.id],
+        "command": "star",
+        "star": prevState.messages[currentStar].starred
+      };
+      const settings = {
+        method: 'PATCH',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      };
+      fetch(`${baseURL}/messages`, settings)
+        .then(response => {
+          if(response.ok){
+            console.log(response);
+          }
+        })
     })
+
   };
   composeButton (){
     this.setState ((prevState) => {prevState.messages.composeForm = !prevState.messages.composeForm})
@@ -62,51 +108,156 @@ export default class App extends Component {
     })
   };
   markAsReadButton (){
+    const msgIds = [];
     this.setState((prevState) => {prevState.messages.map(message => {
       if(message.checked){
         message.read = true;
+        msgIds.push(parseInt(message.id));
       }
-     }
-    )
+     })
+     const data = {
+       "messageIds" : msgIds,
+       "command" : "read",
+       "read" : true
+     };
+     const settings = {
+       method: 'PATCH',
+       headers: {
+         'content-type': 'application/json'
+       },
+       body: JSON.stringify(data)
+     };
+     fetch(`${baseURL}/messages`,settings)
+      .then(response => {
+        console.log(response);
+        if(response.ok){
+        }
+      })
    })
  };
  markAsUnreadButton (){
+   const msgIds = [];
+
    this.setState((prevState) => {prevState.messages.map(message => {
      if(message.checked){
        message.read = false;
+       msgIds.push(message.id);
      }})
+     const data = {
+       "messageIds" : msgIds,
+       "command" : "read",
+       "read" : false
+     };
+     const settings = {
+       method : 'PATCH',
+       headers : {
+         'content-type' : 'application/json'
+       },
+       body: JSON.stringify(data)
+     };
+     fetch(`${baseURL}/messages`,settings)
+      .then(response => {
+        if(response.ok){
+          console.log(response);
+        }
+      })
   })
  };
  addLabel(label) {
+   let msgIds = [];
    this.setState((prevState) => {prevState.messages.map(message => {
      if(message.checked && !message.labels.includes(label)){
        message.labels.push(label)
+       msgIds.push(message.id)
      }
-    })}
-   )
+    })
+    const data = {
+      "messageIds" : msgIds,
+      "command" : "addLabel",
+      "label" : label
+    };
+    const settings = {
+      method: 'PATCH',
+      headers: {
+        'content-type' : 'application/json'
+      },
+      body : JSON.stringify(data)
+    };
+    fetch(`${baseURL}/messages`,settings)
+      .then(response => {
+        if(response.ok){
+          console.log(response);
+        }
+      })
+   })
   };
  removeLabel(label) {
+   let msgIds = [];
    this.setState((prevState) => {prevState.messages.map(message => {
      if(message.checked && message.labels.includes(label)){
        message.labels.splice(message.labels.indexOf(label) ,1);
+       msgIds.push(message.id)
      }
-   })}
-  )
+   })
+   const data = {
+     "messageIds" : msgIds,
+     "command" : "removeLabel",
+     "label" : label
+   };
+   const settings = {
+     method: 'PATCH',
+     headers: {
+       'content-type' : 'application/json'
+     },
+     body: JSON.stringify(data)
+   }
+   fetch(`${baseURL}/messages`,settings)
+    .then(response => {
+      if(response.ok){
+        console.log(response);
+      }
+    })
+  })
  };
  deleteEmail(){
+   let msgIds = [];
    this.setState((prevState) => {prevState.messages= prevState.messages.filter(message => {
      if(!message.checked){
        return message
      }
-   }
-  )})
+     else {
+       msgIds.push(message.id)
+     }
+    })
+    const data = {
+      "messageIds" : msgIds,
+      "command" : 'delete'
+    };
+    console.log(data);
+    const settings = {
+      method : 'PATCH',
+      headers : {
+        'content-type' : 'application/json'
+      },
+      body : JSON.stringify(data)
+    };
+    fetch(`${baseURL}/messages`,settings)
+      .then(response => {
+        if(response.ok){
+          console.log(response);
+        }
+      })
+   })
  }
 
   render() {
     return (
       <div className="container">
         <div className="row">
-          <Toolbar messages={this.state.messages} composeButton={this.composeButton.bind(this)} selectAllButton={this.selectAllButton.bind(this)} markAsReadButton={this.markAsReadButton.bind(this)} markAsUnreadButton={this.markAsUnreadButton.bind(this)} addLabel={this.addLabel.bind(this)} removeLabel={this.removeLabel.bind(this)} deleteEmail={this.deleteEmail.bind(this)}/>
+          <Toolbar messages={this.state.messages} composeButton={this.composeButton.bind(this)}
+                   selectAllButton={this.selectAllButton.bind(this)} markAsReadButton={this.markAsReadButton.bind(this)}
+                   markAsUnreadButton={this.markAsUnreadButton.bind(this)} addLabel={this.addLabel.bind(this)} removeLabel={this.removeLabel.bind(this)}
+                   deleteEmail={this.deleteEmail.bind(this)}/>
           <div>
             <Messages messages={this.state.messages} star={this.star.bind(this)} checkbox={this.checkbox.bind(this)} emailCheck={this.emailCheck.bind(this)}/>
           </div>
